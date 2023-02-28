6e89ad309d51e4733ce6c929a57aee420c80eec6
abdallahsasa <abdallah97sasa@gmail.com>
2023-03-01 01:30:48 +0300

    fix

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
-    protected $table = 'Products';
+    protected $table = 'products';
     protected $fillable = array('name','description','price','active','image');
 
     public function category()
