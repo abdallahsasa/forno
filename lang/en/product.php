e104931eed0c73c4fe6b52f62302ac98b3964af9
abdallahsasa <abdallah97sasa@gmail.com>
2023-03-07 13:44:41 +0300

    fix

--- lang/en/product.php
+++ lang/en/product.php
@@ -4,6 +4,12 @@ return [
 
     'banner-title' => 'More & More Juices',
     'related-products' => 'Need a new flavor adventure? Try these!',
-
+    'Regular' => 'Regular',
+    'Large' => 'Large',
+    '1 Liter' => '1 Liter',
+    '1.5 Liter' => '1.5 Liter',
+    '35 Bottles' => '35 Bottles',
+    '20 Bottles' => '20 Bottles',
+    '12 Pieces' => '12 Pieces',
 ];
 
