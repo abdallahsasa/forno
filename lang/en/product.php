df271b64ef20ef082caba28c730adbb92550f749
abdallahsasa <abdallah97sasa@gmail.com>
2023-03-07 13:53:20 +0300

    fix

--- lang/en/product.php
+++ lang/en/product.php
@@ -8,6 +8,7 @@ return [
     'Large' => 'Large',
     '1Liter' => '1 Liter',
     '1.5Liter' => '1.5 Liter',
+    '1,5Liter' => '1.5 Liter',
     '35Bottles' => '35 Bottles',
     '20Bottles' => '20 Bottles',
     '12Pieces' => '12 Pieces',
